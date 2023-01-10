dbf115d89ca6dd37c3be921a8ff2fdc5af1a3b00
Ricard Sanz <rsanzso@gmail.com>
2023-01-10 15:49:07 +0100

    subir cambios realizados test

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/custom', function() {
+
+    $msj2 = "Mensaje del servidor *-*";
+
+    $data =  ['msj' => $msj2, "edad" => 15];
+    return view('custom' , $data);
+});
